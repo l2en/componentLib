70c7687e2114932684ceb7236c809e4308fe2ebb
luolin <luolin@luolindeMBP.lan>
2019-03-06 17:52:50 +0800

    新增axios.js

--- README.md
+++ README.md
@@ -4,13 +4,28 @@
 + []为可选参数
 
 ----
-
-+ **modalContainer.js**:  以antd的Modal组件作为展示容器，中间可直接放入展示内容（antd版本： 3.7.3）
-
-+ **download.js**: 原生下载功能函数，参数url,[name]；兼容chrome/IE/Firefox
-
-+ **新增scss通用样式**: ex:  mb-20=margin-bottom:20px;per-30=width:30%;
-
+####  **日志**
 [2019-03-06]
 
-+ **loading**: 加载动画，在DOM上使用class='loading'即可![loading](/demo/loading.gif)
++ **新增**：
+  + **axios.js**
+  + **loading.css**
+
+---
+
+
+
+####  **js库**
+
++ **modalContainer.js**:  以antd的Modal组件作为展示容器，中间可直接放入展示内容（antd版本： 3.7.3）
++ **download.js**: 原生下载功能函数，参数url,[name]；兼容chrome/IE/Firefox
++ **axios.js**: 基于axios.js 0.18.0 二次封装，兼容上传file（该封装中content-type为form-data）
+
+---
+
+
+
+
+#### **css库**
++ **styles-->Font.scss/Margin_padding.scss/PerWidth.scss**: 全局通用样式；usage:  mb-20=margin-bottom:20px;per-30=width:30%;
++ **loading.css**：加载动画；usage： DOM中使用class='loader'
---
+++ components/axios.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import * as _ from 'lodash';
+
+import router from '../router/index'
+// axios 配置
+axios.defaults.timeout = 5000
+axios.defaults.baseURL = 'http://mshop.yiyao365.cn'
+
+//设置content-type
+axios.defaults.headers.get['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+/**
+ * 如果是对象就遍历，通过form-data的key-value对应
+ * 如果为对象数组就加key
+ * */
+
+axios.defaults.transformRequest = [function (DTO) {
+  let ret = new FormData();
+  if (!Array.isArray(DTO)) { // post: 如果入参是对象
+    for (let i in DTO) {
+      if(typeof DTO[i] == 'object' && String(Object.prototype.toString.call(DTO[i][0])) !='[object File]'){
+        DTO[i] = JSON.stringify(DTO[i])
+      }
+      if(String(Object.prototype.toString.call(DTO[i][0]))=='[object File]'){
+        DTO[i].forEach(item=>{
+          ret.append('file', item);
+        })
+      }
+      if(typeof DTO[i] == 'string'){
+        ret.append(i, DTO[i]);
+      }
+    }
+  }
+  if (Array.isArray(DTO)) {
+    let retArr = [];
+    DTO.forEach((item, index) => {
+      if (index < DTO.length) {
+        retArr.push(item)
+      };
+    })
+    ret.append(DTO['_key'], JSON.stringify(retArr));
+  }
+  return ret;
+}]
+
+
+// http request 拦截器，再去请求前添加toekn至header
+axios.interceptors.request.use(
+  config => {
+    if (localStorage.getItem('token')) {
+      config.headers.token = localStorage.getItem('token');
+    }
+    config.headers.orgguid = `2`;
+    // config.headers.orgguid = `125`;//测试秒杀
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  })
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    if (response.code == 911) {
+      localStorage.removeItem('token')
+      router.push({
+        name: 'login'
+      })
+    }
+    return response
+  },
+  error => {
+    return Promise.reject(error.response.data)
+  });
+
+export default axios
